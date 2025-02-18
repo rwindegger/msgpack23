11518a0601d7b690d68763321b761b93a5c774e4
Rene Windegger <rene@windegger.wtf>
2025-02-18 23:06:22 +0100

    Add conan package build

--- conanfile.py
+++ conanfile.py
@@ -1,21 +1,37 @@
-# This file is managed by Conan, contents will be overwritten.
-# To keep your changes, remove these comment lines, but the plugin won't be able to modify your requirements
-
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout, CMakeToolchain
+from conan.tools.cmake import cmake_layout, CMakeToolchain, CMakeDeps, CMake
 
-class ConanApplication(ConanFile):
-    package_type = "application"
+class msgpack23(ConanFile):
+    name = "msgpack23"
+    version = "1.0"
+
+    license = "MIT"
+    author = "Rene Windegger <rene@windegger.wtf>"
+    url = "https://github.com/rwindegger/msgpack23"
+    description = "A modern, header-only C++ library for MessagePack serialization and deserialization."
+    topics = ("msgpack", "serialization", "MessagePack")
+
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps"
+
+    exports_sources = ( "CMakeLists.txt", "include/*", "tests/*", "cmake/*" )
 
     def layout(self):
         cmake_layout(self)
 
     def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
         tc = CMakeToolchain(self)
-        tc.user_presets_path = False
         tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
 
     def requirements(self):
         requirements = self.conan_data.get('requirements', [])
