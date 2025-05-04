b9e4f55af284d774199f2c1697063024a7412522
Rene Windegger <rene@windegger.wtf>
2025-05-04 11:25:03 +0200

    Update to Version 3.1

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conan.tools.cmake import cmake_layout, CMakeToolchain, CMakeDeps, CMake
 
 class msgpack23(ConanFile):
     name = "msgpack23"
-    version = "3.0"
+    version = "3.1"
 
     license = "MIT"
     author = "Rene Windegger <rene@windegger.wtf>"
